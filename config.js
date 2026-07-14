6b53dcabdb68f9e6f0a9700c94a13fb8c8900f3c
pjuanfrancisco700-lang <pjuanfrancisco700@gmail.com>
2026-07-13 20:26:06 -0600

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.GAME_CONFIG = {
-  API_URL: "PEGA_AQUI_LA_URL_DE_TU_APPS_SCRIPT",
+  API_URL: "https://script.google.com/macros/s/AKfycby1OtOkEF8x6WSJs_tVwps5dD4u2EppSml9UkZFiwXtkL-wfo_B46webLdR1LdJknY/exec",
   PARTIDA_ID: "PARTIDA-001",
   POLL_INTERVAL_MS: 2500,
   TURN_SECONDS: 45
